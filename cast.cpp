01d691f663c01f6f7c1689d5d613fd3a8a07368f
rrlc <ladvocatrodrigo@gmail.com>
2023-11-22 21:45:52 -0300

    teste

---
+++ cast.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include "vec3.h"
+#include "ray.h"
+#include "color.h"
+
+
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto delta = b*b - 4*a*c;
+    return (delta >= 0);
+}
+
+bool hit_plane(const point3& plane_point, const vec3& plane_vector, const ray& r){
+
+    // dist = ((P0 - R0) DOT PV) / Rdir DOT PV
+
+    if (dot(r.direction(), plane_vector) == 0) {return false;}
+
+    auto dist = (dot((plane_point - r.origin()), plane_vector) / dot(r.direction(), plane_vector));
+    if (dist >= 0) {return true;}
+    else{ return false; }
+
+}
+
+color ray_color(const ray& r) {
+    
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
+
+    if (hit_plane(point3(1,0,-2), vec3(1, 3, 1), r)){
+        return color(0, 1, 0);
+    }
+    
+    return color(0, 0, 0);
+}
+
+int main() {
+
+    // Image
+
+    double aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+
+    auto focal_length = 1.0;    // distance between the viewport and the camera
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);  //top to bottom vector
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    // little steps that go from a pixel's center to another pixel's center
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center + vec3(0, 0, -focal_length) - viewport_u/2 - viewport_v/2; // top left corner
+    auto top_left_pixel = viewport_upper_left + 0.5 * (pixel_delta_v + pixel_delta_u);  //  pixel (0, 0)
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+        for (int i = 0; i < image_width; ++i) {
+            auto pixel_center = top_left_pixel + (i * pixel_delta_u) + (j * pixel_delta_v); // jumping from pixel to pixel with the mini-vectors
+            auto ray_direction = pixel_center - camera_center;
+
+            ray_direction = ray_direction / ray_direction.length();   // making it a unit vector
+
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);   // painting the pixel with the color of the object that the pixel intercepted
+            
+            write_color(std::cout, pixel_color);
+        }
+    }
+}
